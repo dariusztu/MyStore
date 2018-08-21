3eddcc1795a5be3da91ef1f025493d1552056958
dariusztu <dariusztu@gmail.com>
2018-08-21 15:34:15 +0200

    Add getHomePagecartButtonClick method and selector.

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,6 +1,8 @@
 package pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class HomePage {
@@ -11,6 +13,16 @@ public class HomePage {
         this.driver = driver;
         PageFactory.initElements(driver, this);
     }
+
+    @FindBy(css="#header > div:nth-child(3) > div > div > div:nth-child(3) > div > a")
+    private WebElement homePagecartButton;
+
+    public WebElement getHomePagecartButtonClick() {
+        this.homePagecartButton.click();
+        return homePagecartButton;
+    }
+
+
     public void goToHomePage(){
         driver.get("http://automationpractice.com/index.php");
     }
